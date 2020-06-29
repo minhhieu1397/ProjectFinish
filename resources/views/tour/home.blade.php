5d34516c7e6dcd75efd58227a3108ff4749540db
Nguyen Minh Hieu <minhhieu97.hust@gmail.com>
2020-06-30 01:41:35 +0700

    initial rekease

--- resources/views/tour/home.blade.php
+++ resources/views/tour/home.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
         <style>
